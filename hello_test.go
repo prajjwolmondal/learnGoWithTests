db8828f632c0c99ed2ceab92e03ecca26e4db017
prajjwol <prajj.mondal@gmail.com>
2024-06-26 11:40:20 -0400

    refactored code

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,28 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Prajjwol")
-	want := "Hello, Prajjwol"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Prajjwol")
+		want := "Hello, Prajjwol"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+// For helper functions, it's a good idea to accept a testing.TB which is an 
+// interface that *testing.T and *testing.B both satisfy, so you can call 
+// helper functions from a test, or a benchmark
+func assertCorrectMessage(t testing.TB, got, want string) {
+	
+	// t.Helper() is needed to tell the test suite that this method is a helper. 
+	// By doing this, when it fails, the line number reported will be in our 
+	// function call rather than inside our test helper. 
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
