38a86cc5af4047c888ffc1f7895d1e0073b3a0d0
prajjwol <prajj.mondal@gmail.com>
2024-06-26 12:31:53 -0400

    completed first chapter

--- hello_test.go
+++ hello_test.go
@@ -4,14 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Prajjwol")
+		got := Hello("Prajjwol", "")
 		want := "Hello, Prajjwol"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Prajjwol", "Spanish")
+		want := "Hola, Prajjwol"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Prajjwol", "French")
+		want := "Bounjour, Prajjwol"
 		assertCorrectMessage(t, got, want)
 	})
 }
